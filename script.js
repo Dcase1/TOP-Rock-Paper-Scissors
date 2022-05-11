8165965808050993989335e1a25cb8e1fffed08b
Devon <case.devonj@gmail.com>
2022-05-10 20:33:45 -0500

    Create playRound function

--- script.js
+++ script.js
@@ -25,7 +25,30 @@ function correctCase(){
 
 let playerSelection = correctCase(playerChoice);
 
+/* Plays a round of the game, comparing playerSelection to
+computerSelection to determine returned outcome */
 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+        return "You win! Scissors beats Paper!";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper"){
+        return "You lose! Paper beats Rock!";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+        return "You lose! Rock beats Scissors!";
+    } else if (playerSelection === computerSelection){
+        return "Tie!"
+    } else {
+        return "Please select rock, paper, or scissors!"
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
 
 
 
